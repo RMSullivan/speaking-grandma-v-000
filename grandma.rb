d8237ceafbd16751702ff18d99f0559c689b69c6
Rita Sullivan <ritarojas@mac.com>
2018-06-13 13:03:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -9,8 +9,6 @@
 
 # NO, NOT SINCE 1938!
 
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
@@ -18,3 +16,7 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
   end
 end
+
+# However if you say 'I LOVE YOU GRANDMA!', she should respond with
+# 'I LOVE YOU TOO PUMPKIN!'
+
