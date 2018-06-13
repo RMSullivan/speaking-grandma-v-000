c43d92eea70fd2fb9264552477f642c41a980d14
Rita Sullivan <ritarojas@mac.com>
2018-06-13 13:23:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so)
+# If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -15,8 +15,6 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
-  elsif 
-    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
 
