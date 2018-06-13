fc975cda8cf399021be64021c27fdcd1374f5ce7
Rita Sullivan <ritarojas@mac.com>
2018-06-13 13:28:32 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,7 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
-  elsif 
-     phrase == I LOVE YOU GRANDMA!
+  elsif
      return "I LOVE YOU TOO PUMPKIN"
   end
 end
